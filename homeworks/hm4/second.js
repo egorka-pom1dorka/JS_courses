54c97e851336865abe279a43d5e933958c18f0f8
ArtimenyaEgor <megaegor99@gmail.com>
2017-10-15 19:53:10 +0300

    Homework 4. Changed checking phone number

--- homeworks/hm4/second.js
+++ homeworks/hm4/second.js
@@ -1,5 +1,10 @@
 var str = prompt("Enter phone number");
 var pattern = /\+?\s?(375)[\s\-]*[\(\)]?([\d]{2})[\(\)]?[\s\-]*([\d]{2,3})[\s\-]*([\d]{2})[\s\-]*([\d]{2,3})/;
 var results = str.match(pattern);
-var output = '+' + results[1] + results[2] + results[3] + results[4] + results[5];
-console.log(output);
+
+if(results){
+	var output = '+' + results[1] + results[2] + results[3] + results[4] + results[5];
+	console.log(output);
+}else{
+	console.log("Invalide data, try again");
+}
