cba5947a2f961be60479f1f0562b625e3f64d6a3
ArtimenyaEgor <megaegor99@gmail.com>
2017-12-04 21:52:48 +0300

    homework 15

--- homeworks/hm15/script.js
+++ homeworks/hm15/script.js
@@ -1,18 +1,25 @@
 var canvas = document.getElementById('draw-area');
 var ctx = canvas.getContext("2d");
-var x = 0, y = 0;
+var x1 = 0, y1 = 0, x2 = 0, y2 = 0;
 ctx.fillStyle = "#000";
 
 canvas.addEventListener('mousemove', function(e){
 	if(flag){
-		x = e.offsetX;
-		y = e.offsetY;
-		ctx.fillRect(x, y, 2, 2);
+		x2 = e.offsetX;
+		y2 = e.offsetY;
+		ctx.beginPath();
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+		ctx.stroke();
+		x1 = x2;
+		y1 = y2;
 	}
 });
 
 var flag = false;
 canvas.addEventListener('mousedown', function(e){
+	x1 = e.offsetX;
+	y1 = e.offsetY;
 	flag = true;
 });
 canvas.addEventListener('mouseup', function(){
